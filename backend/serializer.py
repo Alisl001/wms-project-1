607be039bccb94af244f11c68efd10052a16cceb
Ali <abofayez.001d@gmail.com>
2024-04-07 16:52:35 +0300

    add the Serializer for all the models

---
+++ backend/serializer.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderDetail, Warehouse, Shelf, Inventory, Shipment, ShipmentDetail, Notification
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetail
+        fields = '__all__'
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = '__all__'
+
+
+class ShelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shelf
+        fields = '__all__'
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class ShipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shipment
+        fields = '__all__'
+
+
+class ShipmentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShipmentDetail
+        fields = '__all__'
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+
