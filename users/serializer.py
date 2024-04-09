01848423e9cafc8442b3da0866863e2e2eb41b54
Ali <abofayez.001d@gmail.com>
2024-04-09 04:20:10 +0300

    Add user register API

---
+++ users/serializer.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    role = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password', 'confirm_password', 'role')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        role = validated_data.pop('role')
+        user = User.objects.create_user(
+            **validated_data,
+            is_staff=(role == 'staff' or role == 'admin'),
+            is_superuser=(role == 'admin'), 
+        )
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import UserRegistrationSerializer
 
-# Create your views here.
+
+#Register API
+@api_view(['POST'])
+def userRegistration(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
